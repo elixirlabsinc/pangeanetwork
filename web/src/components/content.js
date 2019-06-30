b6689de37fd3789e808ec76882331d527eb0441a
Aashni Shah <contact@aashni.me>
2019-06-30 16:09:44 -0400

    WIP positioning content div

--- web/src/components/content.js
+++ web/src/components/content.js
@@ -1,4 +1,15 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const ContentDiv = styled.div`
+  background-color: #F6F9FC;
+`;
+
+const ContentArea = styled.div`
+  margin: auto;
+  width: 80%;
+  padding: 12px;
+`;
 
 class Content extends Component {
   constructor() {
@@ -20,16 +31,18 @@ class Content extends Component {
 
   render() {
     return (
-      <React.Fragment>
-        <p>Content</p>
-        {/* <div>
-          {this.state.data.map(({name, age, city}) => {
-            return (
-              <p key={name}>name: {name}, age: {age}, city: {city}</p>
-            )
-          })}
-        </div> */}
-      </React.Fragment>
+      <ContentDiv>
+        <ContentArea>
+          <p>Content</p>
+          {/* <div>
+            {this.state.data.map(({name, age, city}) => {
+              return (
+                <p key={name}>name: {name}, age: {age}, city: {city}</p>
+              )
+            })}
+          </div> */}
+        </ContentArea>
+      </ContentDiv>
     );
   }
 }
